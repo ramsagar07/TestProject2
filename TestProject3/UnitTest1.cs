8087649bcd303df72afc86747d56f817060da68f
ramsagar07 <129396634+ramsagar07@users.noreply.github.com>
2023-12-06 14:31:59 +0530

    Update UnitTest1.cs

--- TestProject3/UnitTest1.cs
+++ TestProject3/UnitTest1.cs
@@ -22,7 +22,7 @@ namespace TestProject1
             appiumOptions.AddAdditionalCapability("deviceName", "Android Emulator");
             appiumOptions.AddAdditionalCapability("udid", "2C261FDH3003CJ");
             appiumOptions.AddAdditionalCapability("platform Version", "12");
-            appiumOptions.AddAdditionalCapability("app","..\\..\\TestProject2\\dk.resound.smart3d-Signed.apk");
+            appiumOptions.AddAdditionalCapability("app","/home/runner/work/TestProject2/TestProject2/dk.resound.smart3d-Signed.apk");
             appiumOptions.AddAdditionalCapability("appPackage", "dk.resound.smart3d");
             //appiumOptions.AddAdditionalCapability("appActivity", "crc646f76f13f64c213ab.WelcomeScreenActivity");
             appiumOptions.AddAdditionalCapability("Appium Server Address", "127.0.0.1:4723");
